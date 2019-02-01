e25ebdc091813825ae9c5f907b96a242c90e9211
Jiří Patera <jiri.patera@eway-crm.com>
2019-02-01 16:24:11 +0100

    refs #34377: First sample

---
+++ Examples/AcquireContactsByCompany/README.md
@@ -0,0 +1,58 @@
+# Find contacts bound to company with COMPANY or GENERAL type relation
+We would like to find contacts based on their relation with certain company and show them even with foreign keys. Although there is a slight obstacle in our way. We can't use function ```$eway.class->searchContacts()``` because it can't take foreign keys into notice. 
+
+## Correct approach
+
+### Search company and it's relations
+In order to find out which contacts are bound to our chosen company, we need to use function ```$eway.class->searchCompanies()```, use name of our chosen company as first parameter and 'true' as second parameter. The second parameter is indicating, that you want to include relations of searched item. We will find GUIDS of contacts we are looking for there.
+
+```php
+
+//Container for search parameters (FileAs in our case)
+$object = array( 'FileAs' => $company_name );
+    
+//Get data of the company ("true" parameter determines wether we get relation data of searched company)
+$object = $connector->searchCompanies( array( 'FileAs' => $company_name ), true );
+
+```
+
+### Get contact GUIDS
+Now we have ```$object``` containing data of  found company. By calling ```$object->Data[0]->Relations``` we get into relational data of company. All we have to do now is cycle through and pick GUID of related object which in our case must have GENERAL or COMPANY type relation and the lead to contact. 
+
+```php
+
+//List through company relations (relations are actualy on 3rd depth of $object)
+foreach( $object->Data[0]->Relations as $key => $relation ){
+
+    //We are looking for relation which is labeled GENERAL or COMPANY and is leding to Contact
+    if( ($relation->RelationType === 'GENERAL' || $relation->RelationType === 'COMPANY') && $relation->ForeignFolderName === 'Contacts' ){
+        
+        //Store the GUID of the contact
+        array_push( $contacts, $relation->ForeignItemGUID );
+    }
+    
+}
+
+```
+
+### Get contacts
+Now we have array of GUIDS of contacts. We will use it as first parameter and 'true' as second for function ```$connector->getContactsByItemGuids()```. The second parameter indicates that we want to see foreign keys of item we are looking for, which was our primary goal.
+```php
+
+//Get data of contacts
+$contacts = $connector->getContactsByItemGuids( $contacts, true );
+
+```
+
+## Output
+
+### Simple HTML table
+To ease orientation in output of our search we can create simple HTML table. The output should look something like this.
+![example output](eway-crm-php-lib/Images/AcquireContactsByCompany.png)
+
+### Raw output
+Alternatively, you can add ```var_dump($object)``` at the end of the example code to see raw output of company. If you wanted to see detail of company relations, you can add ```var_dump($object->Data[0]->Relations)``` . For raw output of contacts we searched, we would add  ```var_dump($contacts)```  .
+
+## Sample code
+To see the whole sample code click [here](eway-crm-php-lib/AcquireContactsByCompany/sample_code.php)
+
---
+++ Examples/AcquireContactsByCompany/sample_code.php
@@ -0,0 +1,83 @@
+<?php
+    
+    //Variable for our output
+    $table =   '<style type="text/css">
+    
+                    table {
+                        border-collapse: collapse;
+                    }
+                    
+                    th, td {
+                        padding: 15px;
+                        text-align: left;
+                    }
+                    
+                    tr:nth-child(even) {background-color: #f2f2f2;}
+                    
+                </style>';
+
+    //Load API
+    require_once ('eway-crm-php-lib/eway.class.php');
+    
+    //Known company name
+    $company_name = 'Chemel & Peterson LLC';
+    
+    //Connect to API
+    $connector = new eWayConnector( 'https://trial.eway-crm.com/31994/WcfService/Service.svc', 'user1', 'FgYYBY27' );
+    
+    //Container for search parameters (FileAs in our case)
+    $object = array( 'FileAs' => $company_name );
+    
+    //Get data of the company ("true" parameter determines wether we get relation data of searched company)
+    $object = $connector->searchCompanies( array( 'FileAs' => $company_name ), true );
+    
+    //Open <table> tag for our output
+    $table .= '<table align="center">';
+    
+    //Head of our output table
+    $table .=  '<tr>
+                    <td style="border: 1px solid black;"><b>Name</b></td>
+                    <td style="border: 1px solid black;"><b>Adress</b></td>
+                    <td style="border: 1px solid black;"><b>Telephone</b></td>
+                    <td style="border: 1px solid black;"><b>Email</b></td>
+                </tr>';
+                
+    //Container for GUIDS            
+    $contactsGUIDS = array();
+    
+    //List through company relations (relations are actualy on 3rd depth of $object)
+    foreach( $object->Data[0]->Relations as $key => $relation ){
+
+        //We are looking for relation which is labeled GENERAL or COMPANY and is leding to Contact
+        if( ($relation->RelationType === 'GENERAL' || $relation->RelationType === 'COMPANY') && $relation->ForeignFolderName === 'Contacts' ){
+            
+            //Store the GUID of the contact
+            array_push( $contactsGUIDS, $relation->ForeignItemGUID );
+        }
+        
+    }
+    
+    //Get data of contacts
+    $contacts = $connector->getContactsByItemGuids( $contactsGUIDS, true );
+    
+    //List through contacts (data itself are on 2nd depth of object)
+    foreach( $contacts->Data as $contact ){
+        
+        $table .= '<tr>'; //Open new table row
+        
+        //Put the contact information we want into table cells 
+        $table .=  '<td style="border: 1px solid black;">'.$contact->FirstName.' '.$contact->LastName.'</td>
+                    <td style="border: 1px solid black;">'.$contact->BusinessAddressCity.'<br>'.$contact->BusinessAddressStreet.' '.$contact->BusinessAddressPostalCode.'</td>
+                    <td style="border: 1px solid black;">'.$contact->TelephoneNumber1.'</td>
+                    <td style="border: 1px solid black;">'.$contact->Email1Address.'</td>';
+        
+        $table .= '</tr>'; //Close the table row
+    }
+    
+    //Close the <table> tags
+    $table .= '</table>';
+    
+    //Show the table with output
+    echo $table;
+    
+?>
