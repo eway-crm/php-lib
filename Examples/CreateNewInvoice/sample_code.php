fc934b58397b8e4d6a3c49c37de40c1c352ade9c
Jiří Patera <jiri.patera@eway-crm.com>
2019-02-28 13:15:23 +0100

    refs #35062: Visual fix

--- Examples/CreateNewInvoice/sample_code.php
+++ Examples/CreateNewInvoice/sample_code.php
@@ -8,22 +8,22 @@
     
     //This is new cart, that we want to create
     $newCart = array(
-                     'FileAs' => 'Desired Invoice',
-                     'Companies_CustomerGuid' => 'bc0c3aef-64c9-4db5-a739-370937268203',
-                     'Contacts_ContactPersonGuid' => '0db3650f-bb87-4acc-96d6-9e6993cc6e61',
-                     'GoodsInCart' => array(
-										    array(
-										  		  'Goods_GoodsInfoGuid' => '9c09e24a-3901-448f-928e-d2041d327cc7'
-										  		  ),
-										    array(
-												  'FileAs' => 'Service',
-												  'Name' => 'Service',
-												  'Code' => 'WRK-003',
-												  'PriceTotal' => '900'
-												  ),
-                                            ),
-                     'Projects_CartGuid' => '5dac8817-ac48-4469-bae3-41778042a911'
-                     );
+		'FileAs' => 'Desired Invoice',
+		'Companies_CustomerGuid' => 'bc0c3aef-64c9-4db5-a739-370937268203',
+		'Contacts_ContactPersonGuid' => '0db3650f-bb87-4acc-96d6-9e6993cc6e61',
+		'GoodsInCart' => array(
+			array(
+				  'Goods_GoodsInfoGuid' => '9c09e24a-3901-448f-928e-d2041d327cc7'
+				  ),
+			array(
+				  'FileAs' => 'Service',
+				  'Name' => 'Service',
+				  'Code' => 'WRK-003',
+				  'PriceTotal' => '900'
+				  ),
+			),
+		'Projects_CartGuid' => '5dac8817-ac48-4469-bae3-41778042a911'
+		);
     
     //Save the Cart
     $connector->saveCart($newCart);
