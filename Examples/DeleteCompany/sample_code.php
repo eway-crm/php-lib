e2b4b0844a2ba6e3165b7811fb4195cf8c862111
Jiří Patera <jiri.patera@eway-crm.com>
2019-03-04 12:21:14 +0100

    refs #35063: Comments formatting

--- Examples/DeleteCompany/sample_code.php
+++ Examples/DeleteCompany/sample_code.php
@@ -1,15 +1,15 @@
 <?php
 
-    //Load API
+    // Load API
     require_once "eway.class.php";
     
     // Create connector
     $connector = new eWayConnector('https://trial.eway-crm.com/31994', 'api', 'ApiTrial@eWay-CRM');
     
-    //GUID of company
+    // GUID of company
     $company = '7dfde804-2ed4-11e9-be3d-bc5ff40119b6';
     
-    //Delete the company
+    // Delete the company
     $output = $connector->deleteCompany($company);
 
 ?>
